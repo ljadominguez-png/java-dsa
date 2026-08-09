0d17845c6cdf750e8d8ebe44c749d7129123a38e
ljadominguez-png <dominguezlaurencej@gmail.com>
2026-08-09 18:19:33 +0800

    my first tabulation algorithm problem day 20th

---
+++ climbing_stairs.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class climbing_stairs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+
+        System.out.print("Enter number of steps: ");
+        number = input.nextInt();
+
+        input.close();
+
+        //solution
+        climbing_stairs solution = new climbing_stairs();
+        System.out.println("Total number of step needed: " + solution.climbingStairs(number));
+    }
+    // n represents number of steps
+    // recursion does work but it is forbidden due to the TLE constraints
+    // a poposed solution is Tabulation (aka bottom up)
+    public int climbingStairs (int n){
+        //place holder
+         int[] ways = new int[n+1];
+        // in fibonacci sequence it always starts with 1,1,2,3,5,8...
+         ways[0] = ways[1] = 1; 
+         for(int index = 2; index <= n; index++){
+            ways[index] = ways[index-1] + ways[index-2];
+         }
+        return ways[n];
+    }
+}
